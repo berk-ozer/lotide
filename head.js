e392bf318822585bf37f78d7b7a775c41b3d2798
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-03 21:30:34 +0000

    Completed head function

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  return array[0];
+}
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+assertEqual(head([4]), 4);
+assertEqual(head([]), undefined);
