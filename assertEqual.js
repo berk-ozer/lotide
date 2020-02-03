39b8636c8d284ec667066b10014bc8594b0fa2cc
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-03 21:04:14 +0000

    Completed assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+
+  } else {
+    console.log('❌❌❌ Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual('Hello', 'Hello');
+assertEqual('What', 'How');
+assertEqual(42, 42);
+assertEqual(4, 235);
