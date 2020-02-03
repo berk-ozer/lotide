2170daa5431b446c4d5c18ecda3fe50fe8bf5c88
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-03 21:18:35 +0000

    Converted the strings in assertEqual to template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 
   } else {
-    console.log('❌❌❌ Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
