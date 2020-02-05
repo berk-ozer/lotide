c42753277198e16a3bbf82e23eda19570c1fb97c
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-05 20:27:38 +0000

    Set up function to check for equality when the value of the object is an array

--- eqObjects.js
+++ eqObjects.js
@@ -29,7 +29,11 @@ const eqObjects = function(object1, object2) {
 
   if (object1Keys.length === object2Keys.length) {
     for (const key of object1Keys) {
-      if (object1[key] !== object2[key]) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
         return false
       }
     }
