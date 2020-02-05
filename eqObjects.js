5ff3a889eccec43c64b97ebbec1f12eed37be4be
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-05 20:10:07 +0000

    Set up function to check for primitive values

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,4 @@
-// ASSERT FUNCTION
+// ASSERT FUNCTIONS
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -8,9 +8,36 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+
+  return true;
+};
+
 // ACTUAL FUNCTION
 const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
 
+  if (object1Keys.length === object2Keys.length) {
+    for (const key of object1Keys) {
+      if (object1[key] !== object2[key]) {
+        return false
+      }
+    }
+  } else {
+    return false;
+  }
+
+  return true;
 };
 
 // TEST CASES
@@ -20,3 +47,15 @@ const abc = { a: '1', b: '2', c: '3'};
 
 assertEqual(eqObjects(ab, ba), true);
 assertEqual(eqObjects(ab, abc), false);
+
+const person1 = { name: 'Berk', age: 24};
+const person2 = { name: 'Sarah', age: 24};
+
+assertEqual(eqObjects(person1, person2), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+
+assertEqual(eqObjects(cd, dc), true);
+assertEqual(eqObjects(cd, cd2), false);
