825183c7be578d1b22fedb6934ff8f135793fba8
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-07 20:15:32 +0000

    Added test cases for nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -21,6 +21,7 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+console.log('NON NESTED ARRAY TEST');
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
@@ -28,4 +29,10 @@ assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
 assertEqual(eqArrays(['hello', false, 42], ['hello', false, 42]), true);
 assertEqual(eqArrays([false, true], ['', 1]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+
+console.log('\n NESTED ARRAY TEST');
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+assertEqual(eqArrays([[[[[5]]]]], [[[[[5]]]]]), true);
 
