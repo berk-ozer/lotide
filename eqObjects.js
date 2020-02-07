56ec45ea4b58d1ab4cc68bb6a30fe267afff748a
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-07 19:32:47 +0000

    Added test cases for nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -45,6 +45,8 @@ const eqObjects = function(object1, object2) {
 };
 
 // TEST CASES
+console.log('NON-NESTED OBJECT TEST');
+
 const ab = { a: '1', b: '2' };
 const ba = { b: '2', a: '1' };
 const abc = { a: '1', b: '2', c: '3'};
@@ -63,3 +65,11 @@ const cd2 = { c: "1", d: ["2", 3, 4] };
 
 assertEqual(eqObjects(cd, dc), true);
 assertEqual(eqObjects(cd, cd2), false);
+
+console.log('\nNESTED OBJECT TESTS')
+
+// NESTED OBJECT TESTS
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
