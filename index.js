d57b64976bd152735fcf4da1360d9ad90da52e06
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-10 18:50:28 +0000

    Created index.js which exports all of the functions in the library

---
+++ index.js
@@ -0,0 +1,27 @@
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const head = require('./head');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
+}
