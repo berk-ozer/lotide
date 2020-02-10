6246357a80b6471c7a1c8d7e4cf08bb887e91f23
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-10 18:11:24 +0000

    Added mocha and chai for testing. Refactored headTest.js to use m&c

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,20 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-assertEqual(head([4]), 4);
-assertEqual(head([]), undefined);
+describe('#head', () => {
+  it('should return 5 for [5, 6, 7]', () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  })
+
+  it("should return 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  })
+
+  it("should return 4 for [4]", () => {
+    assert.strictEqual(head([4]), 4);
+  })
+
+  it("should return undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  })
+})
