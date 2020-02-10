1a853e7e4e1b3e96698a0e1dc3550815cdf68dfb
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-10 18:27:55 +0000

    Refactored to use mocha and chai for testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,28 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle(['hi', 'hello', 'what']), ['hello']);
-assertArraysEqual(middle([true, false]), []);
-assertArraysEqual(middle(['what', 45, false, 'yes', 22, 3]), [false, 'yes']);
-assertArraysEqual(middle(['one thing']), []);
+describe('#middle', () => {
+  it('should return [2] when passed [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  })
+
+  it('should return [2, 3] when passed [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  })
+
+  it("should return ['hello'] when passed ['hi', 'hello', 'what']", () => {
+    assert.deepEqual(middle(['hi', 'hello', 'what']), ['hello']);
+  })
+
+  it('should return [] when passed [true, false]', () => {
+    assert.deepEqual(middle([true, false]), []);
+  })
+
+  it("should return [false, 'yes'] when passed ['what', 45, false, 'yes', 22, 3]", () => {
+    assert.deepEqual(middle(['what', 45, false, 'yes', 22, 3]), [false, 'yes']);
+  })
+
+  it("should return [] when passed ['one thing']", () => {
+    assert.deepEqual(middle(['one thing']), []);
+  })
+})
