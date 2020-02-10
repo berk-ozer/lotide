3bebaf79ccf4d5bc48c1973a8ff68b8d2757838c
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-10 14:53:37 +0000

    Refactored tail.js to import assertEqual . Created tailTest.js

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   return array.slice(1);
